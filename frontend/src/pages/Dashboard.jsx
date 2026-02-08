dd56119b58caa5af6de277a5b5fea2978266609e
trantrongtin <trantin1973@gmail.com>
2026-02-08 09:32:22 +0700

    update Board FE

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -120,7 +120,7 @@ const Dashboard = ()=>{
                 </h1>
 
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-                    {/* Existing Boards */}
+                    {/* Các bảng hiện có */}
                     {boards.map((board) => (
                         <div 
                             key={board.id}
